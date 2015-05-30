a87d23444fc5e7385cd53bc7fcf55cf226856d4a
Lucas Cotta <lucas.oliveira.cotta@gmail.com>
2015-05-30 16:38:53 -0400

    running the app with the templates express + swig + consolidate

--- express/app.js
+++ express/app.js
@@ -1,10 +1,20 @@
 //reference
 var express = require('express'),
     app = express();
+    cons = require('consolidate');
+
+// assign the swig engine to .html files
+app.engine('html', cons.swig);
+
+//loading the template
+app.set('view engine','html');
+
+//setting the path
+app.set('views',__dirname + "/views");
 
 //route, function > request, response
 app.get('/', function(req, res){
-    res.send("Test");
+    res.render('test', { 'name' : 'Lucas' })
 });
 
 //*any route
